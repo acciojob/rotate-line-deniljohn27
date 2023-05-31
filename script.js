82ea8e1fb0d4c0757c14fcbca51bc4e5a54c6df1
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-31 18:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+window.onload=function(){
+	var line=document.getElementById()"line";
+	var angle=0;
+	var speed=1;
+	fucntion rotateLine(){
+		line.style.transform="rotate(" + angle + "deg)";
+		angle+=speed;
+		requestAnimationFrame(rotateLine);
+	}
+	rotateLine();
+}
